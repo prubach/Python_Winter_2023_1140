33cf48d5a8b5967c1f0b8daf4b8ba9c4312dc365
prubac <pawel.rubach@gmail.com>
2023-10-26 12:04:50 +0200

    vars.py - more

--- vars.py
+++ vars.py
@@ -18,3 +18,29 @@ print(f'cf={cf}')
 
 df = 0.00000000000000005345345234635345
 print(df)
+
+print('----------------------')
+ef = 35345.787872356
+print(int(ef))
+print(round(ef))
+print(round(ef, 2))
+
+print('----------------------')
+ab = 0b1001
+print(ab)
+
+ax = 0x6a
+print(ax)
+
+print('----------------')
+a = 10
+b = 8
+
+c = a / b
+print(type(c))
+print(c)
+
+# Modulo division - rest of division
+d = a % b
+print(type(d))
+print(d)
