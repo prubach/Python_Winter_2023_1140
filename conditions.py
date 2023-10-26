d717a426600da02748359167d20f3dbb209f18ae
prubac <pawel.rubach@gmail.com>
2023-10-26 12:47:17 +0200

    conditions - truth table

---
+++ conditions.py
@@ -0,0 +1,26 @@
+p = False
+q = True
+
+if p and q:
+    print('p and q True')
+else:
+    print('p and q False')
+
+if p or q:
+    print('p or q True')
+else:
+    print('p or q False')
+
+#XOR
+if p ^ q:
+    print('p xor q True')
+else:
+    print('p xor q False')
+
+if not p and q:
+    print('not p and q True')
+else:
+    print('not p and q False')
+
+
+print('-------------')
