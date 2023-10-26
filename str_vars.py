5f3ec6e84ec79f0c1e033eae308fe68d2902783b
prubac <pawel.rubach@gmail.com>
2023-10-26 12:41:44 +0200

    string vars - more

--- str_vars.py
+++ str_vars.py
@@ -34,3 +34,18 @@ s7 = '523453\nsghgs\n2346326\n326236'
 print(s7)
 s7a = s7.splitlines()
 print(s7a)
+
+s8 = 'agabdfdabrt\nsgabfe'
+
+print('count ab: %s' % s8.count('ab'))
+# Throws ValueError if not found
+print('first occurrence of ab: %s ' % s8.index('ab'))
+# Return -1 if not found
+print('find ab: %s' % s8.find('abc'))
+print('last occurrence of ab: %s ' % s8.rindex('ab'))
+
+s9 = '     abcaff agvłąśćóóżżźag     '
+print(s9)
+print(s9.rstrip())
+print(s9.lstrip())
+print(s9.strip())
