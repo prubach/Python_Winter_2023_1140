577ec98ca10c996d1518dc7e0ec09d60dd4c6dfe
prubac <pawel.rubach@gmail.com>
2023-10-26 12:57:02 +0200

    conditions - comparisons, match

--- conditions.py
+++ conditions.py
@@ -1,6 +1,8 @@
 p = False
 q = True
 
+# https://introcs.cs.princeton.edu/java/71boolean/images/truth-table.png
+
 if p and q:
     print('p and q True')
 else:
@@ -24,3 +26,41 @@ else:
 
 
 print('-------------')
+a = 10
+b = 10
+
+if a > b:
+    print('a>b')
+else:
+    print('not a>b')
+
+if a == b:
+    print('a=b')
+else:
+    print('not a=b')
+
+if a >= b:
+    print('a>=b')
+else:
+    print('not a>=b')
+
+s1 = 'abc'
+s2 = 'abcf'
+
+# not equal
+if s1 != s2:
+    print('s1!=s2')
+
+y = 100 if s1==s2 else 10
+print(y)
+
+
+mv = 200
+
+match mv:
+    case 100:
+        print('mv=100')
+    case 200:
+        print('mv=200')
+    case _:
+        print('mv is different')
