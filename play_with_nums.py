6950f9020fff5acba1a54d8c9ac08a0546bd59d0
prubac <pawel.rubach@gmail.com>
2023-11-09 13:11:35 +0100

    Added exercise on numbers - solved

--- play_with_nums.py
+++ play_with_nums.py
@@ -1,3 +1,5 @@
+import math
+
 num_list = [2, 5, 7, 9, 11, 13, 18]
 
 '''
@@ -5,3 +7,23 @@ TODO
 1. Print the subsequent 2nd powers of elements in the list
 2. Sum the 2nd powers of elements of the list
 '''
+
+a = 7
+
+#print(a*a)
+#print(a**2)
+#print(math.pow(a, 2))
+
+for el in num_list:
+    print(el**2)
+
+sum_els = 0
+for el in num_list:
+    sum_els = sum_els + el ** 2
+print(f'Sum: {sum_els}')
+
+######
+print('----------------------')
+num_list_pow = [ x**2 for x in num_list]
+print(num_list_pow)
+print(sum(num_list_pow))
