c02369a12ad11bc79a5a038cb253b3de46356cb2
prubac <pawel.rubach@gmail.com>
2023-11-09 12:31:52 +0100

    Email exercise - partially done

--- email_test.py
+++ email_test.py
@@ -2,7 +2,7 @@ email1 = 'pawel.rubach@sgh.waw.pl'
 email2 = '@sgh.waw.pl'
 email3 = 'pawe@sgh.waw.'
 email4 = 'pawesgh.waw.'
-email5 = 'pawesgh.waw.@.'
+email5 = 'pawesgh.waw.@'
 
 email_list = [email1, email2, email3, email4, email5]
 
@@ -10,11 +10,14 @@ def validate_email(email):
     #TODO - verify email....
     # True if ok, False otherwise
     at_pos = email.find('@')
-    #print(at_pos)
-    if at_pos > 0:
-        return True
-    else:
+    #if email[-1]=='@' or email[-1]=='.':
+    if email[-1] in ['@', '.']:
         return False
+    #print(at_pos)
+    if at_pos < 1:
+        return False
+
+    return True
 
 
 for em in email_list:
