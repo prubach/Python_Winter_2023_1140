0f4023532d1ff9dbede9f4c529266309a3c7a6ea
prubac <pawel.rubach@gmail.com>
2023-11-09 12:14:16 +0100

    Email exercise - partially done

--- email_test.py
+++ email_test.py
@@ -9,7 +9,12 @@ email_list = [email1, email2, email3, email4, email5]
 def validate_email(email):
     #TODO - verify email....
     # True if ok, False otherwise
-    return True
+    at_pos = email.find('@')
+    #print(at_pos)
+    if at_pos > 0:
+        return True
+    else:
+        return False
 
 
 for em in email_list:
