5caf710d03761b115e373e5964f62151b197cf9d
prubac <pawel.rubach@gmail.com>
2023-12-21 13:00:17 +0100

    Variable visibility - functions

---
+++ blocks_functions.py
@@ -0,0 +1,15 @@
+
+def sum_numbers(a=2, b=10):
+    c = 100
+    return a + b + d
+
+d = 100
+a = 5
+b = 8
+print(f'a={a}, b={b}, sum={sum_numbers(a,b)}')
+#print(f'c={c}')
+
+e = 10
+b = 50
+#print(f'e={e}, f={f}, sum={sum_numbers(e)}')
+print(f'e={e}, sum={sum_numbers(e)}')
