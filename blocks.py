4710f0b952dcd1132625ec7374d884ce53386c09
prubac <pawel.rubach@gmail.com>
2023-12-21 12:50:44 +0100

    Variable visibility - blocks

---
+++ blocks.py
@@ -0,0 +1,17 @@
+
+i = 0
+for i in range(4):
+    print(f'i={i}')
+    a = 5
+
+print('loop is done')
+print(f'i={i}')
+print(f'a={a}')
+
+b = 10
+if a > 6:
+    if b < 8:
+        d = 10
+    else:
+        d = 20
+print(f'd={d}')
