057a5a0a4fa83e833ac7e87219f38571d7ff1150
prubac <pawel.rubach@gmail.com>
2023-12-21 11:57:20 +0100

    Bowls - recursion

--- bowls.py
+++ bowls.py
@@ -16,5 +16,15 @@ def sum_of_bowls_loop(n):
     return sum([x for x in range(1, n+1)])
 
 
+def sum_of_bowls_recursive(n):
+    if n == 1:
+        return 1
+    else:
+        res = sum_of_bowls_recursive(n - 1) + n
+        return res
+
+
+
 print('Sum using sequences: {}'.format(sum_of_bowls_sequence(n)))
 print('Sum using loop: {}'.format(sum_of_bowls_loop(n)))
+print('Sum using recursion: {}'.format(sum_of_bowls_recursive(n)))
