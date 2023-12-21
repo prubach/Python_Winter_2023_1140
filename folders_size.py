3794f33dee11b92874630aa2f9054071b89b428a
prubac <pawel.rubach@gmail.com>
2023-12-21 12:44:03 +0100

    Sum of folders with subfolders - solved!

--- folders_size.py
+++ folders_size.py
@@ -12,3 +12,16 @@ a = sum(os.path.getsize(os.path.join(my_folder, f))
 print(f'total size: {a}')
 
 #TODO sum all sizes of files in a given folder including the subfolders and their subfolders etc.
+
+# contributed by Jakub Targalski
+def folder_size(folder):
+    total_size = 0
+    for f in os.listdir(folder):
+        file_path = os.path.join(folder, f)
+        if os.path.isfile(file_path):
+            total_size += os.path.getsize(file_path)
+        elif os.path.isdir(file_path):
+            total_size += folder_size(file_path)
+    return total_size
+
+print(f'Total size of all files in the folder is {folder_size(my_folder)}.')
