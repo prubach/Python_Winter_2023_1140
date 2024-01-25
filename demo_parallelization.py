2327f05edd6ba7fd2aa5ad52111e7ebc66a4e49e
prubac <pawel.rubach@sgh.waw.pl>
2024-01-25 10:41:10 +0100

    Demo parallelization

--- demo_parallelization.py
+++ demo_parallelization.py
@@ -1,7 +1,9 @@
+from concurrent.futures import ProcessPoolExecutor
 from functools import partial
 from multiprocessing import Pool
+from pathlib import Path
 
-from knotprot_download import get_proteins, download_link, setup_download_dir, time_it
+from knotprot_download import get_proteins, download_link, setup_download_dir, time_it, create_thumbnail
 
 
 def run_seq(my_dir):
@@ -16,10 +18,24 @@ def run_seq(my_dir):
 def run_multiprocesssing(my_dir):
     prot_list = get_proteins()
     download = partial(download_link, my_dir)
-    with Pool(8) as pool:
+    with Pool(12) as pool:
         pool.map(download, prot_list)
 
+def thumbnails_seq():
+    for image_path in Path('images').iterdir():
+        print(image_path)
+        create_thumbnail((256, 256), image_path)
+
+
+def thumbnails_parallel():
+    image_list = Path('images').iterdir()
+    create_thumb = partial(create_thumbnail, (256, 256))
+    with ProcessPoolExecutor(max_workers=8) as executor:
+        executor.map(create_thumb, image_list)
 
 if __name__ == '__main__':
     setup_download_dir()
-    time_it(run_seq, 'images')
+    #time_it(run_seq, 'images')
+    time_it(run_multiprocesssing, 'images')
+    #time_it(thumbnails_seq)
+    time_it(thumbnails_parallel)
